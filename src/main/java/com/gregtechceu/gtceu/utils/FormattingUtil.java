6f822f19635693bea98acaa1dc7798a18dac13ea
screret <68943070+screret@users.noreply.github.com>
2024-07-27 15:21:42 +0300

    gui works except tooltip

--- src/main/java/com/gregtechceu/gtceu/utils/FormattingUtil.java
+++ src/main/java/com/gregtechceu/gtceu/utils/FormattingUtil.java
@@ -166,6 +166,11 @@ public class FormattingUtil {
     }
 
     @NotNull
+    public static String formatNumber0Places(float number) {
+        return DECIMAL_FORMAT_0F.format(number);
+    }
+
+    @NotNull
     public static String formatNumber2Places(float number) {
         return DECIMAL_FORMAT_2F.format(number);
     }
